802a40441df341feb18e78b68c0568c1b11ca9c7
Seojin.K <7701seojin@gmail.com>
2025-11-05 04:23:03 +0900

    실습 29
    (실습 14에) 텍스처 입히기
    
    키보드
    c: 육면체
    p: 사각뿔
    x/X: x축을 중심으로 회전
    y/Y: y축을 중심으로 회전
    s: 초기화

---
+++ 2023182003_ComputerGraphics_projects/ComputerGraphics_01_29/vertex.glsl
@@ -0,0 +1,18 @@
+#version 330
+
+layout (location = 0) in vec3 in_Position;
+layout (location = 1) in vec3 in_Color;
+layout (location = 2) in vec2 in_TexCoord;
+out vec3 out_Color;
+out vec2 out_TexCoord;
+
+uniform mat4 modelTransform;
+uniform mat4 projectionTransform;
+uniform mat4 viewTransform;
+
+void main(void) 
+{
+	gl_Position =  projectionTransform * viewTransform * modelTransform * vec4 (in_Position.x, in_Position.y, in_Position.z, 1.0);
+	out_Color = in_Color;
+	out_TexCoord = in_TexCoord;
+}
