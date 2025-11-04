ec13226010f70bada5e42e3156e85b310f4fedc9
Seojin.K <7701seojin@gmail.com>
2025-11-05 04:24:28 +0900

    실습 30
    (실습 27에) 반투명 육면체 추가
    
    키보드
    m: 조명을 켠다/끈다.
    s: 눈이 내린다/멈춘다.
    y: 전체 화면이 y축을 중심으로 공전

---
+++ 2023182003_ComputerGraphics_projects/ComputerGraphics_01_30/vertexShaderSource.glsl
@@ -0,0 +1,21 @@
+#version 330
+
+layout (location = 0) in vec3 in_Position;
+layout (location = 1) in vec3 vNormal;
+
+out vec3 FragPos; //--- 객체의 위치값을 프래그먼트 세이더로 보낸다.
+out vec3 Normal; //--- 노멀값을 프래그먼트 세이더로 보낸다.
+
+uniform int in_Normal;
+uniform mat4 modelTransform;
+uniform mat4 projectionTransform;
+uniform mat4 viewTransform;
+
+void main(void) 
+{
+	gl_Position =  projectionTransform * viewTransform * modelTransform * vec4 (in_Position, 1.0f);
+
+	FragPos = vec3(modelTransform * vec4(in_Position, 1.0));	//--- 객체에 대한 조명 계산을 프래그먼트 셰이더에서 한다.
+	//--- 따라서 월드공간에 있는 버텍스 값을 프래그먼트 셰이더로 보낸다.
+	Normal = vec3(modelTransform * vec4(vNormal, 1.0f));		//--- 노멀값을 프래그먼트 세이더로 보낸다.
+}
