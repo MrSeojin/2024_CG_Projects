7f9052a971c0940468b1af30700d99a3e3c2d361
Seojin.K <7701seojin@gmail.com>
2025-11-05 04:20:38 +0900

    실습 27, 28
    눈 내리는 장면 만들기, 시어핀스키 삼각형
    
    키보드
    s: 하늘에서 눈이 내린다.
    0~5: 시어핀스키 프렉탈 삼각형의 단계
    m: 조명을 켜기/끄기
    r: 조명이 화면 중앙의 y축에 대하여 공전
    c: 조명 색을 다른 색으로 바뀌도록 한다. 3종류의 다른 색을 적용해본다.
    p: 조명을 xz 평면의 좌우상하 위치로 옮긴다.
    n/f: 조명이 화면 중앙에 가까워지기/멀어지기
    +/-: 조명의 세기 높아지기/낮춰지기
    q: 프로그램 종료

---
+++ 2023182003_ComputerGraphics_projects/ComputerGraphics_01_27/fragmentShaderSource.glsl
@@ -0,0 +1,32 @@
+﻿#version 330
+in vec3 FragPos; //--- 위치값
+in vec3 Normal; //--- 버텍스 세이더에서 받은 노멀값
+out vec4 FragColor; //--- 최종 객체의 색 저장
+
+uniform float lightLevel;
+uniform vec3 objectColor;
+uniform vec3 viewPos; //--- 조명의 위치
+uniform vec3 lightPos; //--- 조명의 위치
+uniform vec3 lightColor; //--- 응용 프로그램에서 설정한 조명 색상
+
+void main(void) 
+{	
+	float ambientLight = lightLevel; //--- 주변 조명 계수
+	vec3 ambient = ambientLight * lightColor; //--- 주변 조명 값
+	
+	vec3 normalVector = normalize (Normal);
+	vec3 lightDir = normalize (lightPos - FragPos); //--- 표면과 조명의 위치로 조명의 방향을 결정한다.
+	float diffuseLight = max (dot (normalVector, lightDir), 0.0); //--- N과 L의 내적 값으로 강도 조절: 음수 방지
+	vec3 diffuse = diffuseLight * lightColor; //--- 산란 반사 조명값: 산란반사값 * 조명색상값
+	
+	int shininess = 128; //--- 광택 계수
+	vec3 viewDir = normalize (viewPos - FragPos); //--- 관찰자의 방향
+	vec3 reflectDir = reflect (-lightDir, normalVector); //--- 반사 방향: reflect 함수 - 입사 벡터의 반사 방향 계산
+	float specularLight = max (dot (viewDir, reflectDir), 0.0); //--- V와 R의 내적값으로 강도 조절: 음수 방지
+	specularLight = pow(specularLight, shininess); //--- shininess 승을 해주어 하이라이트를 만들어준다.
+	vec3 specular = specularLight * lightColor; //--- 거울 반사 조명값: 거울반사값 * 조명색상값
+	
+	vec3 result = (ambient + diffuse + specular) * objectColor; //--- 최종 조명 설정된 픽셀 색상: (주변+산란반사+거울반사조명)*객체 색상
+
+	FragColor = vec4 (result, 1.0);
+}
